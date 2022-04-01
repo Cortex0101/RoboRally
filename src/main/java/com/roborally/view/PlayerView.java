2b8b1dd7e3f897d9713cafcb80fe25f03638ac31
Cortex0101 <ldeiruff@gmail.com>
2022-04-01 09:23:43 +0200

    Command display names are now shown in INTERACTION_MODE

--- src/main/java/com/roborally/view/PlayerView.java
+++ src/main/java/com/roborally/view/PlayerView.java
@@ -199,19 +199,15 @@ public class PlayerView extends Tab implements ViewObserver {
                 playerInteractionPanel.getChildren().clear();
 
                 if (player.board.getCurrentPlayer() == player) {
-                    // TODO Assignment V3: these buttons should be shown only when there is
-                    //      an interactive command card, and the buttons should represent
-                    //      the player's choices of the interactive command card. The
-                    //      following is just a mockup showing two options
-                    Button optionButton = new Button("Option1");
-                    optionButton.setOnAction( e -> gameController.notImplemented());
-                    optionButton.setDisable(false);
-                    playerInteractionPanel.getChildren().add(optionButton);
-
-                    optionButton = new Button("Option 2");
-                    optionButton.setOnAction( e -> gameController.notImplemented());
-                    optionButton.setDisable(false);
-                    playerInteractionPanel.getChildren().add(optionButton);
+                    Command command = player.getProgramField(player.board.getStep()).getCard().command;
+                    if (command.isInteractive()) {
+                        for (Command option : command.getOptions()) {
+                            Button button = new Button(option.displayName);
+                            button.setOnAction(e -> gameController.notImplemented());
+                            button.setDisable(false);
+                            playerInteractionPanel.getChildren().add(button);
+                        }
+                    }
                 }
             }
         }
