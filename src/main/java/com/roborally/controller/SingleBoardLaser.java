5f245f64db1df5db53649b73fcfd8507b1aefc1b
Cortex0101 <ldeiruff@gmail.com>
2022-06-01 10:31:14 +0200

    Created two walls and created boardlaser class

---
+++ src/main/java/com/roborally/controller/SingleBoardLaser.java
@@ -0,0 +1,12 @@
+package com.roborally.controller;
+
+import com.roborally.model.Player;
+import com.roborally.model.Space;
+
+public class SingleBoardLaser extends FieldAction {
+
+  @Override
+  public boolean doAction(GameController gameController, Space space) {
+    return false;
+  }
+}
