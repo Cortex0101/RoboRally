cc460bd65e12020b54e8dc22aefb44f918b01375
Cortex0101 <ldeiruff@gmail.com>
2022-06-18 08:07:19 +0200

    Made the textInputDialog return empty string by default

--- src/main/java/com/roborally/view/DialogFacade.java
+++ src/main/java/com/roborally/view/DialogFacade.java
@@ -17,25 +17,62 @@ public class DialogFacade {
     return alert;
   }
 
+  /**
+   * @author Lucas Eiruff
+   *
+   * Prompts the user with a confirmation alert
+   *
+   * @param title the dialog's title
+   * @param text the dialog's text
+   * @return true if the user pressed ok, false otherwise.
+   */
   public static boolean newConfirmationAlert(String title, String text) {
     Alert alert = newAlert(AlertType.CONFIRMATION, title, text);
     Optional<ButtonType> result = alert.showAndWait();
     return result.isEmpty() || !result.get().equals(ButtonType.OK);
   }
 
+  /**
+   * @author Lucas Eiruff
+   *
+   * Prompts the user with an information alert
+   *
+   * @param title the dialog's title
+   * @param text the dialog's text
+   * @return true if the user pressed ok, false otherwise.
+   */
   public static boolean newInformationAlert(String title, String text) {
     Alert alert = newAlert(AlertType.INFORMATION, title, text);
     Optional<ButtonType> result = alert.showAndWait();
     return result.isEmpty() || !result.get().equals(ButtonType.OK);
   }
 
+  /**
+   * @auhor Lucas Eiruff
+   *
+   * Prompts the user with a text dialog
+   *
+   * @param title the dialog's title
+   * @param text the dialog's text
+   * @return the text the user entered or an empty string if the user entered nothing
+   */
   public static String newTextInputDialog(String title, String text) {
     TextInputDialog dialog = new TextInputDialog();
     dialog.setTitle(title);
     dialog.setHeaderText(text);
-    return dialog.showAndWait().orElseThrow();
+    return dialog.showAndWait().orElse("");
   }
 
+  /**
+   * @author Lucas Eiruff
+   *
+   * Prompts the user with a choice dialog.
+   *
+   * @param choices the choices present in the dialog
+   * @param title the dialog's title
+   * @param text the dialog's text
+   * @return the opotion the user chose
+   */
   public static String newChoiceDialog(List<String> choices, String title, String text) {
     assert !choices.isEmpty();
     ChoiceDialog<String> dialog = new ChoiceDialog<>(choices.get(0), choices);
