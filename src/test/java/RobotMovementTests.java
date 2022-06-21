6d6afbce664a88919bd41778051e8953d01b0ac8
Cortex0101 <ldeiruff@gmail.com>
2022-06-21 12:29:05 +0200

    Implemented all robot movement tests

--- src/test/java/RobotMovementTests.java
+++ src/test/java/RobotMovementTests.java
@@ -3,10 +3,13 @@ import com.roborally.controller.GameController;
 import com.roborally.model.Board;
 import com.roborally.model.Command;
 import com.roborally.model.CommandCard;
+import com.roborally.model.Heading;
 import com.roborally.model.Player;
 import java.util.List;
+import java.util.Objects;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RobotMovementTests {
@@ -35,10 +38,115 @@ public class RobotMovementTests {
     game.executePrograms();
   }
 
+  private class Position {
+    Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public boolean is(int x, int y) {
+      return this.x == x && this.y == y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Position position = (Position) o;
+      return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
+    }
+
+    public int x;
+    public int y;
+  }
+
+  private Position getPos(Player player) {
+    return new Position(player.getSpace().x, player.getSpace().y);
+  }
+
+  @Test
+  void turnLeftTest() {
+    performMoves(List.of(new CommandCard(Command.LEFT)), player1);
+
+    Position expectedPosition = new Position(0, 2);
+    Heading expectedHeading = Heading.EAST;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
+  }
+
+  @Test
+  void turnRightTest() {
+    performMoves(List.of(new CommandCard(Command.RIGHT)), player1);
+
+    Position expectedPosition = new Position(0, 2);
+    Heading expectedHeading = Heading.WEST;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
+  }
+
+  void uTurnTest() {
+    performMoves(List.of(new CommandCard(Command.LEFT)), player1);
+
+    Position expectedPosition = new Position(0, 2);
+    Heading expectedHeading = Heading.NORTH;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
+  }
+
   @Test
   void move1Test() {
-    Assertions.assertTrue(player1.getSpace().x == 0 && player1.getSpace().y == 2);
     performMoves(List.of(new CommandCard(Command.MOVE1)), player1);
-    Assertions.assertTrue(player1.getSpace().x == 0 && player1.getSpace().y == 3);
+
+    Position expectedPosition = new Position(0,3);
+    Heading expectedHeading = Heading.SOUTH;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
+  }
+
+  @Test
+  void move2Test() {
+    performMoves(List.of(new CommandCard(Command.MOVE1), new CommandCard(Command.LEFT), new CommandCard(Command.MOVE2)), player1);
+
+    Position expectedPosition = new Position(2,3);
+    Heading expectedHeading = Heading.EAST;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
+  }
+
+  @Test
+  void move3Test() {
+    performMoves(List.of(new CommandCard(Command.MOVE1), new CommandCard(Command.LEFT), new CommandCard(Command.MOVE3)), player1);
+
+    Position expectedPosition = new Position(3,3);
+    Heading expectedHeading = Heading.EAST;
+    Position actualPosition = getPos(player1);
+    Heading actualHeading = player1.getHeading();
+
+    Assertions.assertEquals(actualPosition, expectedPosition);
+    Assertions.assertSame(actualHeading, expectedHeading);
   }
 }
