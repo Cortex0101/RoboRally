75cbd5ca7eff5278fcf002add81d4193f6d0841b
Cortex0101 <ldeiruff@gmail.com>
2022-07-15 10:18:50 +0200

    First attempts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.roborally {
+open module com.roborally {
   requires javafx.controls;
   requires javafx.base;
   requires javafx.graphics;
@@ -6,9 +6,14 @@ module com.roborally {
   requires org.jetbrains.annotations;
   requires com.google.gson;
   requires java.desktop;
+  requires spring.boot.autoconfigure;
+  requires spring.boot;
+  requires spring.web;
 
-  opens com.roborally to javafx.fxml;
-  opens com.roborally.controller to com.google.gson;
+
+  //opens com.roborally to javafx.fxml, spring.boot, spring.web, spring.boot.autoconfigure;
+  //opens com.roborally.RESTapi to spring.web, spring.boot, spring.boot.autoconfigure;
+  //opens com.roborally.controller to com.google.gson;
   exports com.roborally;
   exports com.roborally.fileaccess.model to com.google.gson;
   exports com.roborally.controller;
